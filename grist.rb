c3df19beab2493b26053c57cecd3080f35472604
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2012-04-12 22:08:45 +1000

    Clean up Grist class, remove commented crap and cruft

--- grist.rb
+++ grist.rb
@@ -6,13 +6,8 @@ end
 
 class Grist
   def initialize repo_path
-    @repo = build_repo repo_path
+    @repo = Grit::Repo.new repo_path
   end
-
-  def build_repo repo_path
-    Grit::Repo.new repo_path
-  end
-  private :build_repo
 
   def repo
     @repo
@@ -20,10 +15,7 @@ class Grist
 
   def display_content hash
     repo.blob(hash).data
-    #%x[cd rep/ && git cat-file -p #{hash}]
   end
-
-
 
   def save content, branch = nil, filename = 'temp.txt'
     if branch && branch.empty?
@@ -34,70 +26,27 @@ class Grist
 
     blob_hash = repo.git.put_raw_object content, 'blob'
 
-    #pp "BLOB-HASH #{blob_hash}"
-
-#  tree         = "10644 #{blob_hash} #{filename}"
-#    cmd = "git update-index --add --cacheinfo 100644 #{blob_hash} #{filename} ]"
     repo.git.native :update_index,  {}, '--add', '--cacheinfo' ,  '10644', blob_hash, filename
-    tree_hash = repo.git.native :write_tree
-    #tree_hash    = %x[cd rep/ && git write-tree ]
-    #pp "TREE-HASH #{tree_hash}"
 
-    #abbrev_hash = commit_hash.slice 0,7
+    tree_hash = repo.git.native :write_tree
     if branch and !branch.empty?
-      #pp 'GOT-BRANCH'
-      #pp branch
       #TODO get all prev commits, convert to Grit
       p_commit_hash = %x[ cd rep/ && git show-ref --heads --hash #{branch}]
       p_commit_hash.chomp!
-      pre = " -p #{p_commit_hash}"
+      previous_commits = " -p #{p_commit_hash}"
     else
-      pre = ''
+      previous_commits = ''
     end
 
-    #commit_hash = repo.commit_index 'Writing tree as commit :: FOO'
-    #commit_hash = repo.commit_objects
-    #commit_hash  = %x[cd rep/ &&  echo 'Writing tree as commit -- ' | git commit-tree #{tree_hash} #{pre} ]
-    commit_hash = repo.git.native :commit_tree, {}, tree_hash, pre
-
+    commit_hash = repo.git.native :commit_tree, {}, tree_hash, previous_commits
 
     branch ||= commit_hash.slice 0,7
-    #prev_branch = branch ? "-p #{branch}" : ''
-    #
-#  update_ref   = %x[cd rep/ &&  git update-ref refs/heads/master #{commit_hash} ]
-#    update_ref   = %x[cd rep/ &&  git update-ref refs/heads/#{branch} #{commit_hash} ]
     update_ref = repo.git.native :update_ref,  {}, "refs/heads/#{branch}", commit_hash
-
-#git update-ref refs/heads/0eac1cf5a890119d0c93a4b4f4d3069887fa767f 0eac1cf5a890119d0c93a4b4f4d3069887fa767f
-
     branch
   end
 
   def log
     %x[cd rep/ &&  git log ]
   end
-
-
-end
-
-
-def gexec command
-  return `pwd`
-end
-
-
-def load_branch_tip branch
-
-end
-
-
-
-
-
-
-
-
-def load branch_hash
-#  commit =
 
 end
